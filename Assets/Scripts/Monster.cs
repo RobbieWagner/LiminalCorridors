9d4d3dc07df26f292f884a4f9370452b188f1363
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-12-15 19:16:34 -0600

    #3 complete

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class Monster : MonoBehaviour
     private GameObject playerGO;
     [SerializeField]
     private LayerMask playerLayer;
+    [SerializeField]
+    private NavMeshAgent navMeshAgent;
+
+    [SerializeField]
+    private float maxDistanceFromPlayer;
 
     enum monsterState{
         inactive,
@@ -25,19 +31,58 @@ public class Monster : MonoBehaviour
 
     // Update is called once per frame
     private void Update(){
+        Vector3 monsterDestination;
+
         if(currentState == (int) monsterState.inactive){
-            if(IsPlayerVisible()) currentState = (int) monsterState.chasingPlayer;
-            Debug.Log("Chase");
+            if(IsPlayerVisible()){
+            currentState = (int) monsterState.chasingPlayer;
+            Debug.Log("Chasing");
+            }
         }
+        else if(currentState == (int) monsterState.roaming){
+            Roam();
+        }
+        else if(currentState == (int) monsterState.chasingPlayer){
+            navMeshAgent.SetDestination(playerGO.transform.position);
+            if(!IsPlayerVisible()){
+            currentState = (int) monsterState.lookingForPlayer;
+            Debug.Log("looking for player");
+            }
+        }
+        else if(currentState == (int) monsterState.lookingForPlayer){
+            float distanceLeft = navMeshAgent.remainingDistance;
+            if(IsPlayerVisible()){
+                currentState = (int) monsterState.chasingPlayer;
+            }
+            else if(distanceLeft < .01f){
+                currentState = (int) monsterState.roaming;
+                Debug.Log("roaming");
+            }
+        }
+
     }
 
     private bool IsPlayerVisible (){
         RaycastHit hit = new RaycastHit();
         Ray ray = new Ray(transform.position, playerGO.transform.position-transform.position);
         Physics.Raycast(ray, out hit, Mathf.Infinity);
-        Debug.Log("Ray Hit: " + hit.transform.name);
-        if(hit.collider.gameObject.layer == playerLayer) return true;
+        if(hit.collider.gameObject.GetInstanceID() == playerGO.GetInstanceID()) return true;
         
         return false;
     } 
+
+    private void Roam(){
+        if(!navMeshAgent.hasPath)
+        {
+            Debug.Log("finding new path");
+            float positionX = Random.Range(-maxDistanceFromPlayer, maxDistanceFromPlayer);
+            float positionZ = Random.Range(-maxDistanceFromPlayer, maxDistanceFromPlayer);
+            Vector3 path = new Vector3(playerGO.transform.position.x + positionX, 0, playerGO.transform.position.z + positionZ);
+            navMeshAgent.SetDestination(path);
+            Debug.Log("path: " + path.ToString());
+        }
+        if(IsPlayerVisible()){
+            currentState = (int) monsterState.chasingPlayer;
+        }
+    }
 }
